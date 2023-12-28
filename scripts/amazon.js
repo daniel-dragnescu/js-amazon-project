e45ca7dcba02ba6429dbdb296515a0b75bef799d
DanielDragnescu <dragnescu_daniel12@yahoo.com>
2023-12-28 14:47:37 +0000

    Use products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -2,31 +2,7 @@
 //create an array because represents an list
 //this is called data structure (combination of arrays and objects)
 
-const products = [{
-image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-rating: {
-  stars: 4.5,
-  count: 87
-},
-priceCents: 1090
-}, {
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars: 4,
-    count: 127
-  },
-  priceCents: 2095
-}, {
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 4.5,
-    count: 56
-  },
-  priceCents: 799
-}];
+//FIRST STEP IS IN PRODUCTS.JS // 
 
 //second step: generate the html
 //it takes each object, save in the parameter product and then run the function
