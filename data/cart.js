5193448c95391ebf94937fb5ff0fcc5909206a41
DanielDragnescu <dragnescu_daniel12@yahoo.com>
2023-12-30 13:33:32 +0000

    Generate HTML for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,10 @@
-export const cart = []; //accesed outisde of this file/ used outside of this js
+export const cart = [{
+  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity: 2,
+}, {
+  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity: 1
+}]; //accesed outisde of this file/ used outside of this js
 
 export function addToCart(productId) {
   let matchingItem; // it is set undefined
@@ -10,7 +16,7 @@ export function addToCart(productId) {
   });
 
   if(matchingItem) {
-    matchingItem.quantity += 1; //it it's in the cart, increase the quantity
+    matchingItem.quantity += 1; //if it's in the cart, increase the quantity
   } else {
     cart.push({
       productId: productId,
