582ec7ff09a11ac680f2fc1a5a0a49d0b808612d
DanielDragnescu <dragnescu_daniel12@yahoo.com>
2023-12-31 11:55:15 +0000

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,24 @@
-export let cart = [{
-  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 2,
-}, {
-  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
-}]; //accesed outisde of this file/ used outside of this js
+export let cart = JSON.parse(localStorage.getItem('cart')); //we need to get the cart from local storage. we need one string, and we have to get the value back from a string with parse
+
+//if not cart/we dont have a cart in local storage, json.parse we give as null
+//in this case, not null will be truthy and will run the code inside the {}
+//if the cart get the value null, we run the code from {}. we have null, so we run the {}
+if(!cart) {
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+  }, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+  }];
+}
+ //accesed outisde of this file/ used outside of this js
+//cart is variable, so it resets when we reload the page or go to different page. we need to localstorage
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));  //local storage needs 2 strings
+}  // first: 'cart' - the name that wherever we want to save / we want to put
+  //second: is the value that we need, but converted into a string by json.stringify
 
 export function addToCart(productId) {
   let matchingItem; // it is set undefined
@@ -23,6 +37,8 @@ export function addToCart(productId) {
       quantity: 1
     });  //if it's not in the cart, add it to the cart
   }
+
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -35,4 +51,6 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToStorage(); //now, whenever we update the card, we save it in localstorage
 }
